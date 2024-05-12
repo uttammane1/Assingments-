958140892c8175c2dd8f281a7824ad8bd56918e4
Uttam Mane <151371801+uttammane1@users.noreply.github.com>
2024-05-12 19:49:23 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,56 +1,27 @@
+import {useState} from 'react';
+import React from 'react';
+
 import './App.css';
-import React,{useState, useEffect} from 'react';
+
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState([]);
-  const [page, setPage] = useState(1); // Pagination state
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(
-          `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
-        );
-        const data = await response.json();
-        setPosts(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const[count, setCount] = useState(0);
 
-    fetchPosts();
-  }, [page]); // Effect depends on page
+   function increment(){
+    setCount(count +1);
+   }
 
+   function decrement(){
+    setCount(count -1);
+   }
   return (
-    <>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <div>
-            {posts.map((post) => (
-              <div key={post.id}>
-                <h3>{post.title}</h3>
-                <p>{post.body}</p>
-              </div>
-            ))}
-          </div>
-          <button
-            onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
-          >
-            Previous
-          </button>
-          <button onClick={() => setPage((prevPage) => prevPage + 1)}>
-            Next
-          </button>
-        </>
-      )}
-    </>
+     <div>
+      <h1>Counter : {count}</h1>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+  </div>
   );
 }
-
 
 export default App;
